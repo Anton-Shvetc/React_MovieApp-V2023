1437af7b23e4c44d58f00d673652763f5a5c2dc7
Anton <antonshvetswork@yandex.ru>
2023-09-18 23:37:31 +0300

    Add films to TopFilmsPage

---
+++ src/pages/MovieCard.tsx
@@ -0,0 +1,39 @@
+// import { GlobalContext } from "../GlobalContext/GlobalState";
+// import { Link } from "react-router-dom";
+// import { MovieControls } from "../components/MovieControls";
+
+interface MovieCardI {
+  nameRu: string;
+  posterUrl: string;
+  ratingImdb: number;
+  ratingKinopoisk: number;
+}
+
+export const MovieCard = ({
+  nameRu,
+  posterUrl,
+  ratingImdb,
+  ratingKinopoisk,
+}: MovieCardI) => {
+  // const { ShowMoreInfo } = useContext(GlobalContext);
+
+  return (
+    <div>
+      <div className="movie-card">
+        {/* <Link to="/moreinfo" onClick={() => ShowMoreInfo(movie)}>
+          <div className="movie-link"> </div>
+        </Link> */}
+        <div className="overlay"> </div>
+        <div className="movie-card_title">{`${nameRu}`}</div>
+
+        <img src={posterUrl} alt=""></img>
+
+        {/* <MovieControls type={type} movie={movie} /> */}
+        <div className="movie-card_average">
+          <p>Рейтинг IMBD: {ratingImdb ? ratingImdb : "ND"}</p>
+          <p>Рейтинг: {ratingKinopoisk}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/TopFilmsPage.tsx
+++ src/pages/TopFilmsPage.tsx
@@ -1,3 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { MovieCard } from "./MovieCard";
+
 export const TopFilmsPage = () => {
-  return <>TopFilmsPage</>;
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  const [filmsData, setFilmData] = useState<any>();
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        `https://kinopoiskapiunofficial.tech/api/v2.2/films`,
+        {
+          headers: {
+            "X-API-KEY": api_key,
+          },
+        }
+      );
+
+      setFilmData(response.data.items);
+    } catch {}
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+console.log(filmsData);
+  return (
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">Фильмы</h1>
+        </div>
+        <div></div>
+        <div className="movie-grid">
+          {filmsData &&
+            filmsData.map((filmItem : any) => {
+              return (
+                <MovieCard
+                  key={filmItem.kinopoiskId}
+                  {...filmItem}
+                  // key={items.id}
+                  // type="topseries"
+                  // name={items.name}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
